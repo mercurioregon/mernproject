9a5ea57931cb1a4f81419eb7265ff9b04d908425
mercurioregon <jamesm620@gmail.com>
2024-07-24 14:21:04 -0700

    Folders and server.js

---
+++ reactfullstack/server/server.js
@@ -0,0 +1,38 @@
+const express = require ("express")
+const path = require("path")
+const {ApolloServer} = require("@apollo/server")
+const {expressMiddleware}=require("@apollo/server")
+
+const {typeDefs, resolvers}= require("./schemas")
+
+const PORT = process.env.PORT || 3001
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+}
+)
+const db =require("./config")
+const app = express()
+
+const startApollo = async ()=>{
+        await server.start()
+        app.use(express.urlencoded({ extended: false }));
+        app.use(express.json());
+        app.use("/graphql", expressMiddleware(server, {
+            context:authMiddleware
+        }))
+
+
+if (process.env.NODE_ENV=="production")
+    {
+        app.use(express.static(path.join(__dirname, '../client/dist')));
+        app.get("*", (req,res) =>{
+            res.sendFile(path/join(__dirname, '../client/dist/index.html'));
+        })
+    }
+        db.once("open", ()=>{
+            app.listen(PORT, () =>{
+            console.log ("currently running on ", PORT)
+        }  )
+      })
+    }

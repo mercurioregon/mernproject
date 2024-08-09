1d3a5b3647a33496cff5b5426923f3d8915ce998
mercurioregon <jamesm620@gmail.com>
2024-08-08 18:19:39 -0700

    app.jsx

--- reactfullstack/client/src/App.jsx
+++ reactfullstack/client/src/App.jsx
@@ -1,35 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css"
+import {Outlet} from "react-router-dom"
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  CreateHttpLink,
 
-function App() {
-  const [count, setCount] = useState(0)
+} from "@apollo/client"
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Uncle + Five</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Don't do anything just yet.  I'm not quite ready. 
-      </p>
-    </>
-  )
-}
-
-export default App
+import {setContext} from "@apollo/client/link/context"

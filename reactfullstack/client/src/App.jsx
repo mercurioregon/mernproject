10830bb314d94d7fafc5bd3e8e36d014188e510d
mercurioregon <jamesm620@gmail.com>
2024-08-11 15:22:19 -0700

    app.jx token

--- reactfullstack/client/src/App.jsx
+++ reactfullstack/client/src/App.jsx
@@ -9,3 +9,28 @@ import {
 } from "@apollo/client"
 
 import {setContext} from "@apollo/client/link/context"
+const backendLink = CreateHttpLink({uri:"/graphql"})
+const auth = setContext((_, {headers})=>{
+  const token= localStorage.getItem("id_token")
+  return{
+    headers:{
+      ...headers,
+      authorization: token? `Bearer ${token}`: ``
+    }
+  }
+})
+
+
+const client = new ApolloClient({
+  link: auth.concat(backendLink)
+
+})
+function App() {
+  return (
+    <ApolloProvider client = {client}>
+
+    </ApolloProvider>
+    
+
+  )
+}
